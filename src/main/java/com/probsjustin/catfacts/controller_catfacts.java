ef0235bb59eabcb25315ec7793cfb4c034508707
Justin Hagerty <wheelsmanx@gmail.com>
2021-08-18 17:44:07 -0400

    added more to the controller to parse the servlet request

--- src/main/java/com/probsjustin/catfacts/controller_catfacts.java
+++ src/main/java/com/probsjustin/catfacts/controller_catfacts.java
@@ -1,5 +1,33 @@
 package com.probsjustin.catfacts;
 
-public class controller_catfacts {
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+public class controller_catfacts {
+	private static String holder_catFact = "";
+	private static String holder_toPhoneNumber = "";
+	private static String holder_fromPhoneNumber = "";
+	private static String holder_debug = "";
+
+	
+	controller_catfacts(){
+
+	}
+	
+	void checkRequestParameter_Validator() {
+		
+	}
+	
+	static HttpServletResponse parseRequest(HttpServletRequest func_request, HttpServletResponse func_response) {
+		holder_catFact = ""; 
+		holder_toPhoneNumber = func_request.getParameter("toPhoneNumber"); 
+		holder_fromPhoneNumber = func_request.getParameter("fromPhoneNumber");
+		holder_debug = func_request.getParameter("debug"); 
+		
+		return func_response;
+	}
 }
--- src/main/java/com/probsjustin/catfacts/servlet_catfacts.java
+++ src/main/java/com/probsjustin/catfacts/servlet_catfacts.java
@@ -7,34 +7,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class servlet_catfacts
- */
 @WebServlet("/servlet_catfacts")
 public class servlet_catfacts extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
     public servlet_catfacts() {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		try {
+			response = controller_catfacts.parseRequest(request, response); 
+		} catch (Exception e){
+			response.setStatus(400);
+			e.printStackTrace();
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		try {
+			response = controller_catfacts.parseRequest(request, response); 
+		} catch (Exception e){
+			response.setStatus(400);
+			e.printStackTrace();
+		}
 	}
 
 }
